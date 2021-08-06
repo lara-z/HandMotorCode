479549b1de211e3b3e46e139f0e4c60ac4895eef
lara-z <84350864+lzlok@users.noreply.github.com>
2021-08-06 12:24:27 -0400

    updated test_UR5 with arm name and getch

--- UR5/test_UR5_commands.py
+++ UR5/test_UR5_commands.py
@@ -1,10 +1,9 @@
 import airobot as ar
 from airobot import Robot
 
-
-robot = ar.Robot('ur5e_2f140', pb=False, use_cam=False)
-z_move = 0.1 # !!! verify number
-wrist_index = 0 # !!! verify number
+robot = ar.Robot('ur5e', pb=False, use_cam=False)
+z_move = 0.05 # !!! verify number
+wrist_index = -1 # !!! verify number
 wrist_move = 0.1 # !!! verify number
 
 pos, quat, rot, euler = robot.arm.get_ee_pose()
@@ -12,18 +11,36 @@ print('this is the robot\'s current Cartesian pose:)
 print(pos)
 
 print('press \'m\' to move arm up by a small amount or ESCAPE to pass)
-if keypress == b'\x1b':
+keypress = getch()
+if keypress == chr(0x1b):
 	print('Escaped from moving')
 	break
-elif keypress == b'm':
+elif keypress == 'm':
 	pos[2] += z_move
 	robot.arm.move_ee_xyz(pos, wait=True)
 	
 print('press \'r\' to rotate the wrist by a small amount or ESCAPE to pass)
-if keypress == b'\x1b':
+if keypress == chr(0x1b):
 	print('Escaped from moving')
 	break
-elif keypress == b'm':
+elif keypress == 'm':
 	rot[wrist_index] += wrist_move
 	robot.arm.set_jpos(rot, wait=True)
+	
+def getch():
+	import os
+
+	if os.name == 'nt':
+		import msvcrt
+		return msvcrt.getch().decode()
+	else:
+		import sys, tty, termios
+		fd = sys.stdin.fileno()
+		old_settings = termios.tcgetattr(fd)
+		try:
+			tty.setraw(sys.stdin.fileno())
+			ch = sys.stdin.read(1)
+		finally:
+			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+		return ch}
 
