3e637149fa75986eba30c6152693148f2c18f113
lara-z <84350864+lzlok@users.noreply.github.com>
2021-08-13 12:29:29 -0400

    changed joint angle pose

--- UR5/test_UR5_commands.py
+++ UR5/test_UR5_commands.py
@@ -24,20 +24,27 @@ wrist_index = -1 # !!! verify number
 wrist_move = 0.1 # !!! verify number
 
 pos, quat, rot, euler = robot.arm.get_ee_pose()
-print('this is the robot\'s current Cartesian pose:)
+print('this is the robot\'s current Cartesian pose:')
 print(pos)
 
-print('press \'m\' to move arm up by a small amount or ESCAPE to pass')
+print('')
+
+jpos = robot.arm.get_jpos()
+print('this is the robot'\'s current joint angle pose:')
+print(jpos)
+
+print('press \'m\' to move arm up by a small amount to location printed below or ESCAPE to pass')
+pos[2] += z_move
+print(pos)
 keypress = getch()
 if keypress == chr(0x1b):
 	print('Escaped from moving')
 elif keypress == 'm':
-	pos[2] += z_move
 	robot.arm.move_ee_xyz(pos, wait=True)
 	
 print('press \'r\' to rotate the wrist by a small amount or ESCAPE to pass')
 if keypress == chr(0x1b):
 	print('Escaped from moving')
 elif keypress == 'm':
-	rot[wrist_index] += wrist_move
-	robot.arm.set_jpos(rot, wait=True)
+	jpos[wrist_index] += wrist_move
+	robot.arm.set_jpos(jpos, wait=True)
